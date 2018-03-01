7bd48c13ac0f7de31bba600e46b1720cf18fa5c3
lijiang0703 <li_jiang0703@outlook.com>
2018-03-01 11:09:53 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var md = require('markdown-it')();
-var str = "### hello"
-
-// module.exports = function(md, options) {
-	
-// }
-md.block.ruler.after("heading",'h_attr',function(state){
-	console.log(state)
-	var tokens = state.tokens;
-})
-
-md.render(str)
+module.exports = function(md, options) {
+	md.core.ruler.after("block",'h_attr',function(state){
+		var tokens = state.tokens;
+		console.log(state)
+	})	
+}
